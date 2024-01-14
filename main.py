70098d17f9698aa1ba1f672c6e0371682bab1ffe
Filigram <130276494+Filigram@users.noreply.github.com>
2024-01-14 13:51:49 +0400

    Add or update file via git

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.edge.options import Options
 from myby import by
+
+class PyQuackr:
+
+    def __init__(self, headless: bool = True) -> None:
+        options = Options()
+        if headless:
+            options.add_argument("headless")
+        else:
+            options.add_argument("start-maximized")
+
+        self.driver = webdriver.Edge(options=options)
+        self.driver.get("https://quackr.io/")
+
+        
