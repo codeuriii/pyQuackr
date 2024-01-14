c25204fd457f721f945c94b84b8def2df5cadb21
Filigram <130276494+Filigram@users.noreply.github.com>
2024-01-14 14:10:29 +0400

    Add or update file via git

--- pyquackr.py
+++ pyquackr.py
@@ -14,5 +14,23 @@ class PyQuackr:
         self.driver = webdriver.Edge(options=options)
         self.driver.get("https://quackr.io/")
 
+    def select_country(self, contry: str):
+        pays = [
+            'australia', 'austria', 'belgium', 'brazil', 'canada',
+            'china', 'denmark', 'finland', 'france', 'germany',
+            'hungary', 'india', 'indonesia', 'ireland', 'israel',
+            'italy', 'korea', 'latvia', 'mexico', 'morocco', 'netherlands',
+            'norway', 'pakistan', 'poland', 'russia', 'south-africa',
+            'spain', 'sweden', 'switzerland', 'ukraine', 'united-kingdom',
+            'united-states'
+        ]
+
+        if not contry in pays:
+            raise ValueError(f"{contry} is not in {pays}\nPlease select a valid country.")
+        
+        url = "https://quackr.io/temporary-numbers/" + contry
+        self.driver.get(url)
+
+
     def get_temporary_numbers(self, lenght: int):
-        print(lenght)
+        pass
